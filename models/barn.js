febb8b776db633915a2af77de2596b3a75ea5650
Abigail Dawson <abigaildawson.dev@gmail.com>
2023-12-02 11:12:24 -0500

    Add addressSchema and barnNoteSchema to Barn model

---
+++ models/barn.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const addressSchema = new Schema({
+    houseNumber: String,
+    street: String,
+    city: String,
+    state: String,
+    zip: Number
+});
+
+const barnNoteSchema = new Schema({
+    dateAdded: Date,
+    content: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const barnSchema = new Schema({
+    name: String,
+    address: {
+        type: [addressSchema],
+        required: true
+    },
+    contacts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Contact'
+    }],
+    horses: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Horse'
+    }],
+    chores: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Contact'
+    }],
+    notes: [barnNoteSchema],
+}, {
+    timestamps: true
+})

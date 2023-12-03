929243f84a0b229ec62aa465be69b35bbbf27f0f
Abigail Dawson <abigaildawson.dev@gmail.com>
2023-12-03 10:12:19 -0500

    Update seed file to associate barns with horses

--- crud-helper.js
+++ crud-helper.js
@@ -29,24 +29,5 @@ const Horse = require('./models/horse');
 // Top-level await (using await outside of an async function)
 // has been available since Node v14.8
 
-
 let barns = await Barn.find({});
 let horses = await Horse.find({})
-horses[0].barns.push(barns[0])
-await horses[0].save()
-horses[1].barns.push(barns[0])
-await horses[1].save()
-horses[2].barns.push(barns[0])
-await horses[2].save()
-horses[3].barns.push(barns[1])
-await horses[3].save()
-horses[4].barns.push(barns[1])
-await horses[4].save()
-horses[5].barns.push(barns[1])
-await horses[5].save()
-horses[6].barns.push(barns[2])
-await horses[6].save()
-horses[7].barns.push(barns[2])
-await horses[7].save()
-horses[8].barns.push(barns[2])
-await horses[8].save()
--- seed.js
+++ seed.js
@@ -7,43 +7,38 @@ const Horse = require('./models/horse');
 
 const data = require('./data');
 
-const seedDB = async () => {
-    try {
-        await Barn.deleteMany({});
-        await Horse.deleteMany({});
+(async function() {
+    const promise1 = Barn.deleteMany({});
+    const promise2 = Horse.deleteMany({});
+    
+    // Promise.all will return a single promise that resolves only after all of the array's promises resolve
+    let results = await Promise.all([promise1, promise2]);
+    // results will be an array of result objects!
+    console.log('results after promise1 and promise2: ', results);
+  
+    // This time, provide the array of promises in-line
+    results = await Promise.all([
+      Barn.create(data.barns),
+      Horse.create(data.horses)
+    ]);
 
-        const addedHorses = await Horse.insertMany(seedHorses);
-        const barnsWithHorses = [
-            { ...seedBarns[0], 
-                horses: [
-                    addedHorses[0]._id, 
-                    addedHorses[1]._id,
-                    addedHorses[2]._id,
-                ] 
-            },
-            { ...seedBarns[1], 
-                horses: [
-                    addedHorses[3]._id,
-                    addedHorses[4]._id,
-                    addedHorses[5]._id,
-                ] 
-            },
-            { ...seedBarns[2], 
-                horses: [
-                    addedHorses[6]._id,
-                    addedHorses[7]._id,
-                    addedHorses[8]._id,
-                ] 
-            }
-        ];
+    // Push horse IDs into barns
+    results[0][0].horses.push(results[1][0]._id, results[1][1]._id, results[1][2]._id);
+    results[0][1].horses.push(results[1][3]._id, results[1][4]._id, results[1][5]._id);
+    results[0][2].horses.push(results[1][6]._id, results[1][7]._id, results[1][8]._id);
 
-        // Insert barns with references to horses
-        await Barn.insertMany(barnsWithHorses);
-    } catch (err) {
-        console.error(err);
-    }
-};
+    // Push barn IDs into horses
+    results[1][0].barns.push(results[0][0]._id);
+    results[1][1].barns.push(results[0][0]._id);
+    results[1][2].barns.push(results[0][0]._id);
+    results[1][3].barns.push(results[0][1]._id);
+    results[1][4].barns.push(results[0][1]._id);
+    results[1][5].barns.push(results[0][1]._id);
+    results[1][6].barns.push(results[0][2]._id);
+    results[1][7].barns.push(results[0][2]._id);
+    results[1][8].barns.push(results[0][2]._id);
 
-seedDB().then(() => {
-    mongoose.connection.close();
-});
+    console.log(results[1][0]);
+    
+    process.exit();
+})();

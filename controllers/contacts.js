e0c3b7924fdd28887bd706660c9d3910a7f6f8ab
Abigail Dawson <abigaildawson.dev@gmail.com>
2023-12-04 09:25:47 -0500

    Implement index functionality for contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,10 +1,22 @@
 const Barn = require('../models/barn');
 const Contact = require('../models/contact');
+const Horse = require('../models/horse');
 
 module.exports = {
+    index,
     addToContacts
 };
 
+async function index(req, res) {
+    const contacts = await Contact.find({})
+        .populate({path: 'horses', model: Horse})
+        .populate({path: 'barns', model: Barn})
+        console.log(contacts[5]);
+    res.render('contacts/index', {
+        title: 'All Contacts',
+        contacts
+    })
+}
 async function addToContacts(req, res) {
     const barn = await Barn.findById(req.params.id);
     barn.contacts.push(req.body.contactId);
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const contactsCtrl = require('../controllers/contacts');
 
+// GET /contacts
+router.get('/contacts', contactsCtrl.index);
+
 // POST /barns/:id/contacts
 router.post('/barns/:id/contacts', contactsCtrl.addToContacts);
 

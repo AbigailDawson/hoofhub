dbe939020fdbd17529aeabd8bdf36e21ac496ae1
Abigail Dawson <abigaildawson.dev@gmail.com>
2023-12-11 10:52:01 -0500

    Style details page for horses

--- controllers/horses.js
+++ controllers/horses.js
@@ -45,6 +45,7 @@ async function index(req, res) {
 async function show(req, res) {
     const horse = await Horse.findById(req.params.id).populate('barns');
     const barns = await Barn.find({});
+    console.log('HORSE: ', horse)
     res.render('horses/show', {
         horse,
         barns,
